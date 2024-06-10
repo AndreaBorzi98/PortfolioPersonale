b21194a4d8d8d208245f9e996c277d77c51bbd49
AndreaBorzi98 <borzi.andrea98@gmail.com>
2024-06-10 23:44:45 +0200

    porftolio modifiche

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const nav = document.getElementById('nav');
+
+mobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.toggle('is-active');
+    nav.classList.toggle('active');
+});
