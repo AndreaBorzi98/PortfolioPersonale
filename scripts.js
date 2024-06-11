1e91a99d43ec9312e400f0fa00a413d58f5a7355
AndreaBorzi98 <borzi.andrea98@gmail.com>
2024-06-11 11:22:51 +0200

    modifiche

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
-const mobileMenu = document.getElementById('mobile-menu');
+/*const mobileMenu = document.getElementById('mobile-menu');
 const nav = document.getElementById('nav');
 
 mobileMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('is-active');
     nav.classList.toggle('active');
+});*/
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const nav = document.getElementById('nav');
+
+    mobileMenu.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    });
 });
+
 
