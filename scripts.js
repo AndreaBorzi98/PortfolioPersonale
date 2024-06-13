9a0fe10b96a653c96b5826251dae9da49f7262b5
AndreaBorzi98 <borzi.andrea98@gmail.com>
2024-06-13 12:24:46 +0200

    modifiche responsive

--- scripts.js
+++ scripts.js
@@ -1,10 +1,3 @@
-/*const mobileMenu = document.getElementById('mobile-menu');
-const nav = document.getElementById('nav');
-
-mobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.toggle('is-active');
-    nav.classList.toggle('active');
-});*/
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const mobileMenu = document.getElementById('mobile-menu');
